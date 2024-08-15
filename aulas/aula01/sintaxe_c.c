f03cf9b968c5189b1560380645c7347fa801557c
luisbiato <luisf.biato@gmail.com>
2024-08-15 00:49:21 +0000

    codigos da aula 1

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,38 @@
+//ISSO eh diferente de isso
+//area_retangulo = base * altura
+/*
+  blocos{ }
+
+main(){
+  linha_de_codigo;
+}*/
+
+  # include <stdio.h>    // Inclusao de bibliotecas
+  
+  int main() {    // função principal
+  //seu codigo aqui
+
+  //1. entrada
+  //levantar meus ingredienes
+  //char, int,float
+  //variaveis
+  //leitura
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 5;
+  segundo_numero = 6;
+  
+  //2. processamento
+  //calculo soma, subtracao, multiplicacao, divisao
+  int soma = primeiro_numero + segundo_numero;
+  
+  
+  //3. saida 
+  //escrita
+  printf("A soma de %i com %i eh %i\n", primeiro_numero, segundo_numero, soma);
+  
+    return 0;    // return fim do programa
+  }
+
+
+
